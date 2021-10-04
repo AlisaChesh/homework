2f4b7660f1b7064a90647018378d58bc8f944bc0
Alisa <alisachrome@gmail.com>
2021-10-04 22:58:34 +0100

    Hello

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
 
 function calculator ($num1, $num2,$action) {
-	if ($action == "+") {  //здесь не знаю нужна ли переменная, чтобы вернуть ее в конце. Если да, тогда не знаю как вписать в нее action
+	if($action == "+") {  //здесь не знаю нужна ли переменная, чтобы вернуть ее в конце. Если да, тогда не знаю как вписать в нее action
 		return $num1 + $num2;
-	} elseif ($action== "-") {
-		return $num1 - $num2 ;
-	} elseif ($action == "*"){
-		return $num1 * $num2;
-	} elseif ($action == "/" && $num1 >= 1 || $num2 >= 1){   //не знаю только как указать, условия если $num1 и 2 >0
-		return $num1 / $num2;
-	} elseif ( $action == "/" && $num2 == 0) {
-		return "К сожалению мы не можем делить число на 0 ¯\_(ツ)_/¯";
-			}
 	}
+	elseif($action == "-") {
+		return $num1 - $num2;
+	}
+	elseif($action == "*") {
+		return $num1 * $num2;
+	}
+	elseif($action == "/" && $num1 >= 1 || $num2 >= 1) {   //не знаю только как указать, условия если $num1 и 2 >0
+		return $num1 / $num2;
+	}
+	elseif($action == "/" && $num2 == 0) {
+		return "К сожалению мы не можем делить число на 0 ¯\_(ツ)_/¯";
+	}
+}
 
 
 echo calculator (50, 35, "/")
